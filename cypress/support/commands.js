6ba5016ef6f3ebae3a6cdc2cafc7e6badbac2823
Finn Jacobs <finn.jacobs@mobilepricecard.com>
2024-02-16 16:55:06 -0500

    added check to avoid login process if already logged in

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,23 +1,33 @@
 Cypress.Commands.add('disableCache', () => {
-  cy.clearCookies();
-  cy.clearLocalStorage();
-  cy.intercept('*', (req) => {
-    req.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate';
-    req.headers['Pragma'] = 'no-cache';
-    req.headers['Expires'] = '0';
-  });
+    cy.clearCookies();
+    cy.clearLocalStorage();
+    cy.intercept('*', (req) => {
+        req.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate';
+        req.headers['Pragma'] = 'no-cache';
+        req.headers['Expires'] = '0';
+    });
 })
 
 Cypress.Commands.add('login', () => {
     cy.visit(Cypress.env('BASE_URL'));
-    cy.origin('https://dev-w0f53seg.us.auth0.com', () => {
-      cy.get('#email').type(Cypress.env('USER_EMAIL'));
-      cy.get('#password').type(Cypress.env('USER_PASSWORD'));
-      cy.get('#btn-login').click();
+
+    // Check if already logged in
+    cy.location('href').then((currentUrl) => {
+        if (currentUrl === Cypress.env('https://dev-w0f53seg.us.auth0.com')) {
+            cy.origin('https://dev-w0f53seg.us.auth0.com', () => {
+                cy.get('#email').type(Cypress.env('USER_EMAIL'));
+                cy.get('#password').type(Cypress.env('USER_PASSWORD'));
+                cy.get('#btn-login').click();
+            });
+        }
     });
 });
 
-// Timoput of 10000ms to give time
 Cypress.Commands.add('openNav', () => {
-  cy.get('#nav-text-collapse > ul.navbar-nav.align-items-center.ml-md-auto > li.nav-item.d-xl-none', {timeout: 10000}).click();
+    cy.get('#nav-text-collapse > ul.navbar-nav.align-items-center.ml-md-auto > li.nav-item.d-xl-none', {timeout: 10000}).click();
+})
+
+Cypress.Commands.add('getPage', (pageName) => {
+    cy.openNav();
+    cy.get(`a[href="#/${pageName}"]`).click().wait(500);
 })
