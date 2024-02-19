8049069a743454e3fa6a5773d3a20771a6813ae1
Finn Jacobs <finn.jacobs@mobilepricecard.com>
2024-02-19 11:16:48 -0500

    cleanup

--- cypress/e2e/super-admin-v8.cy.js
+++ cypress/e2e/super-admin-v8.cy.js
@@ -2,17 +2,14 @@ describe('Super Admin v8', () => {
     
     beforeEach(() => {
         cy.clearLocalStorage();
-    })
+    });
 
     it("should add a new carrier", () => {
         // Login to Phoenix
         cy.login();
     
-        // Open navbar
-        cy.openNav();
-
-        // Navigate to carrier
-        cy.get('.navbar-inner > ul > li > div > ul > a:nth-child(1)').click().wait(500);
+        // Navigate to Carrier
+        cy.getPage('carrier');
 
         // Open add carrier modal
         cy.get('div.card-header > div > div.col-md-4 > div > button').click().click();
@@ -21,26 +18,35 @@ describe('Super Admin v8', () => {
         cy.fixture('super-admin-v8').then(data => {
             cy.get('#term-group-1 > div > #value').type(data.carrier.name);
             cy.get('#value-group-1 > div > #value').type(data.carrier.description);
-        })
+        });
         cy.get('#modal-add___BV_modal_footer_ > button.btn-primary').click();
-    })
+    });
 
     it("should create a new price plan", () => { 
-        // Open navbar
-        cy.openNav();
-      
-        // Navigate to price plans
-        cy.get('.navbar-inner > ul > li > div > ul > a:nth-child(5)').click().wait(500);
+        // Navigate to Price Plans
+        cy.getPage('Priceplan');
 
         // Open add price plan modal
         cy.get('.card-header.text-center > div > div:nth-child(3) > div > button').click().click();
-        
+
         // Fill form and submit
         cy.fixture('super-admin-v8').then(data => {
             cy.get('#pricingPlanName').type(data.pricePlan.name);
             cy.get('#description').type(data.pricePlan.description);
-            cy.get('select#idcarrier').select(data.carrier.name);
-        })
+    
+            // Handle when dropdown has more than one matching option
+            cy.get('select#idcarrier').then($select => {
+                const $options = $select.find(`option`).filter((i, el) => {
+                    return el.textContent === data.carrier.name;
+                });
+                if ($options.length > 0) {
+                    const valueOfFirstMatch = $options.first().val();
+                    cy.wrap($select).select(valueOfFirstMatch);
+                } else {
+                    cy.log(`No option found matching ${data.carrier.name}`);
+                }
+            });
+        });
         cy.get('#modal-add___BV_modal_footer_ > button.btn.btn-primary').click();
-    })
+    });
 });
