a5f5831023315a59fff58974d21a505a7fd8789e
Anneika Weeks <anneikanicole@gmail.com>
2024-02-22 14:32:01 -0500

    getting main back to where it was with ou report and pricing tests also included.

---
+++ cypress/e2e/ou-report.cy.js
@@ -0,0 +1,16 @@
+describe('ou report download', () => {
+    it('should download an OU Report and confirm the download was received', () => {
+      cy.login();
+
+      cy.intercept('GET', `${Cypress.env('BASE_URL')}/Dashboard/OULevelExport`).as('downloadRequest');
+
+      // Trigger the download action
+      cy.get(`a[href="${(Cypress.env('BASE_URL'))}/Dashboard/OULevelExport"]`).click()
+      
+      // Wait for the download request to complete
+      cy.wait('@downloadRequest').then((interception) => {
+        expect(interception.response.statusCode).to.equal(200);
+      });
+
+    })
+  }) 
