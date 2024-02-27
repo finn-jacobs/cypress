40a831944017d2e781a87d904fe6d0a91e60fc6e
Anneika Weeks <anneikanicole@gmail.com>
2024-02-27 16:05:22 -0500

    refactoring different tests to include fixtures and commands

--- cypress/e2e/ou-report.cy.js
+++ cypress/e2e/ou-report.cy.js
@@ -1,20 +1,14 @@
 describe('ou report download', () => {
-    it('should download an OU Report and confirm the download was received', () => {
-        cy.login();
+  it('should download an OU Report and confirm the download was received', () => {
+    cy.login();
+    cy.interceptApiCall('GET', 'Dashboard/OULevelExport');
 
-        cy.intercept('GET', `${Cypress.env('BASE_URL')}/Dashboard/OULevelExport`).as('downloadRequest');
-
-      // Trigger the download action (e.g., clicking a download button)
-      cy.get(`a[href="${(Cypress.env('BASE_URL'))}Dashboard/OULevelExport"]`).click()
-      
-      // Wait for the download request to complete
-      cy.wait('@downloadRequest').then((interception) => {
-        expect(interception.response.statusCode).to.equal(200);
-      });
-
-        // Wait for the download request to complete
-        cy.wait('@downloadRequest').then((interception) => {
-            expect(interception.response.statusCode).to.equal(200);
-        });
+    // Trigger the download action (e.g., clicking a download button)
+    cy.get(`a[href="${(Cypress.env('BASE_URL'))}Dashboard/OULevelExport"]`).click()
+    
+    // Assert 
+    cy.wait('@OULevelExport').then(({response}) => {
+      expect(response.statusCode).to.eq(200);
     });
+  });
 });
--- cypress/e2e/pricing.cy.js
+++ cypress/e2e/pricing.cy.js
@@ -1,154 +1,138 @@
 describe('pricing testing', () => {
-it('should create a new product', () => {
-        cy.interceptApiCall('POST', 'ProductStatic/addProductStatic');
-        cy.login();
+  it('should create a new product', () => {
+      cy.interceptApiCall('POST', 'ProductStatic/addProductStatic');
+      
+      cy.login();
+      cy.getPage('ProductStatic');
 
-        // Navigate to Product Static
-        cy.getPage('ProductStatic');
+      // Press the 'Add' Button
+      cy.get('button.btn.base-button.btn-outline-default')
+          .contains('Add')
+          .click().click();
 
-        // Press the 'Add' Button
-        cy.get('button.btn.base-button.btn-outline-default').contains('Add').click().click();
+      // Open 'Add Product' Modal and fill out all fields
+      cy.fixture('super-admin-v8').then(data => {
+          cy.handleDropdown('#idcarrier', data.carrier.name);
+          cy.handleDropdown('#aspectRatiAddEditModalo', data.product.ar);
+          cy.handleDropdown('#mfg.custom-select.custom-select-sm', data.product.mfg);
+          cy.get('#productName').type(data.product.name);
+          cy.get('#mdescLong').type(data.product.meta);
+          cy.get('#sku').type(data.product.sku);
+      });
 
-        // Fill out 'Add Product' Modal and submit 
-        cy.fixture('super-admin-v8').then(data => {
-            cy.handleDropdown('#idcarrier', data.carrier.name);
-            cy.handleDropdown('#aspectRatiAddEditModalo', data.product.ar);
-            cy.handleDropdown('#mfg.custom-select.custom-select-sm', data.product.mfg);
-            cy.get('#productName').type(data.product.name);
-            cy.get('#mdescLong').type(data.product.meta);
-            cy.get('#sku').type(data.product.sku);
-        });
+      // Submit and assert
+      cy.get('button.btn.btn-primary').click()
+      cy.wait('@addProductStatic').then(({response}) => {
+          const body = JSON.parse(response.body);
+          expect(response.statusCode).to.eq(200);
+          expect(body.error).to.eq(false);
+      })
+  });
 
-        // Submit and assert
-        cy.get('button.btn.btn-primary').click();
-        cy.wait('@addProductStatic').then(({ response }) => {
-            const body = JSON.parse(response.body);
-            expect(response.statusCode).to.eq(200);
-            expect(body.error).to.eq(false);
-        });
-    });
-
-    it('should upload a file and a new product should be created', () => {
-        cy.interceptApiCall('POST', 'ProductStatic/uploadProduct1');
-        cy.login();
-
-        // Navigate to Product Static
-        cy.getPage('ProductStatic');
-
-        // Open Upload Product modal
-        cy.get('button.btn.base-button.mr-1.btn-upload.btn-outline-success').contains('Upload Product').click();
+  it('should upload a file and a new product should be created', () => {
+      cy.interceptApiCall('POST', 'ProductStatic/uploadProduct1')
+      
+      // Open Upload Product modal
+      cy.get('button.btn.base-button.mr-1.btn-upload.btn-outline-success')
+          .contains('Upload Product')
+          .click();
 
       // Upload file
       const filePath = 'cypress/assets/sample_product.xlsx';
-      
       cy.get('label[for="importfile"]').selectFile(filePath);
-      cy.get('button.btn.btn-primary').click();
 
-      // confirmation message should pop up
-      cy.on('window:alert', (message) => {
-      expect(message).to.equal('1 product record(s) added successfully');
+      // Submit and assert
+      cy.get('button.btn.btn-primary').click();
+      cy.wait('@uploadProduct1').then(({response}) => {
+          const body = JSON.parse(response.body);
+          expect(response.statusCode).to.eq(200);
+          expect(body.error).to.eq(false);
       });
-  
   });
 
   it('should check that both new products have red dots on the price card column', () => {
-      // Check the price card column for the last 2 products created
-      // Those elements should be red
-      cy.wait(1000);
-      cy.get('i.fas.fa-circle').then($elements => {
-          const lastTwoElements = $elements.slice(-2);
-          cy.wrap(lastTwoElements).should('have.css', 'color', 'rgb(255, 0, 0)');
+      // Check the last products on the last page
+      cy.get('button[aria-label="Go to last page"]').should('be.visible').click()
+        .then(() => {
+        // Select the last two elements
+        // check their CSS properties
+        cy.get('i.fas.fa-circle').then($elements => {
+            const lastTwoElements = $elements.slice(-2);
+            cy.wrap(lastTwoElements).should('have.css', 'color', 'rgb(255, 0, 0)');
+        });
       });
   });
 
   it('should select one of the new products and create a default price fluid on it', () => {
-      cy.get('td[aria-colindex="2"][role="cell"]').contains('Cypress Carrier').click();
-      cy.wait(1000);
-      cy.get('button.btn.base-button.btn-outline-default').contains('Add').click();
+    cy.interceptApiCall('POST', 'ProductFluid/addProductFluid1*');
 
-      const today = new Date();
-      const twoDaysFromNow = new Date(today);
-      twoDaysFromNow.setDate(today.getDate() + 2);
+    cy.get('td[aria-colindex="2"][role="cell"]').contains('Cypress Carrier').click();
+    cy.wait(1000)
+    cy.get('button.btn.base-button.btn-outline-default').contains('Add').click();
+    
+    // Choose start and end dates
+    cy.handleDatePicker('label#example-datepicker__value_', '#timeExpires__value_');
 
-      // choose 2 dates - today and 2 days from today in calendars
-      cy.get('label#example-datepicker__value_').then($label => {
-        cy.wrap($label).click();
-        const chosenDate = today.toISOString().split('T')[0];
-        cy.get(`[data-date="${chosenDate}"]`).click();
-      });
-
-      cy.get('label#timeExpires__value_').then($label => {
-        cy.wrap($label).click();
-        const formattedDate = twoDaysFromNow.toISOString().split('T')[0];
-        cy.get(`[data-date="${formattedDate}"]`).click();
-      });
-
-      // fill out the rest of the form then press save
-      cy.get('select#pricingPlanID').select('Cypress Price Plan');
-      cy.get('input#planpricename').type('Cypress Test');
-      cy.get('label[for="isDefault"]').click();
-      cy.get('input#planAprice0').type('120.00');
-      cy.get('input#planAprice1').type('10.00');
-      cy.get('input#planAprice2').type('12');
-      cy.get('button.btn.btn-primary').click();
+    // Fill out form        
+    cy.fixture('super-admin-v8').then(data => {
+        cy.handleDropdown('select#pricingPlanID', data.pricePlan.name);
+        cy.get('#planpricename').type(data.product.pricing.name);
+        cy.get('label[for="isDefault"]').click();
       
-      // confirmation message should pop up
-      cy.on('window:alert', (message) => {
-        expect(message).to.equal('Product Fluid added successfully');
-      });
+        data.product.pricing.priceFluids[0].forEach((priceFluid, index) => {
+            cy.get(`#planAprice${index}`).type(priceFluid);
+        });
+        cy.get('button.btn.btn-primary').contains('Ok').click()
+    });
+
+    // Assert
+    cy.wait('@addProductFluid1*').then(({response}) => {
+        const body = JSON.parse(response.body);
+        expect(response.statusCode).to.eq(200);
+        expect(body.error).to.eq(false);
+    });
   });
 
   it('should add a second product fluid to that product', () => {
+    cy.interceptApiCall('POST', 'ProductFluid/addProductFluid1*');
     cy.get('button.btn.base-button.btn-outline-default').contains('Add').click();
 
-    const today = new Date();
-    const twoDaysFromNow = new Date(today);
-    twoDaysFromNow.setDate(today.getDate() + 2);
+    // Choose start and end dates
+    cy.handleDatePicker('label#example-datepicker__value_', '#timeExpires__value_');
 
-    // choose 2 dates - today and 2 days from today in calendars
-    cy.get('label#example-datepicker__value_').then($label => {
-      cy.wrap($label).click();
-      const chosenDate = today.toISOString().split('T')[0];
-      cy.get(`[data-date="${chosenDate}"]`).click();
-    });
-
-    cy.get('label#timeExpires__value_').then($label => {
-      cy.wrap($label).click();
-      const formattedDate = twoDaysFromNow.toISOString().split('T')[0];
-      cy.get(`[data-date="${formattedDate}"]`).click();
-    });
-
-    // fill out the rest of the form then press save
-    cy.get('select#pricingPlanID').select('Cypress Price Plan');
-    cy.get('input#planpricename').type('Cypress Test 2');
-    cy.get('input#planAprice0').type('240.00');
-    cy.get('input#planAprice1').type('20.00');
-    cy.get('input#planAprice2').type('12');
-    cy.get('button.btn.btn-primary').click();
-    
-    // confirmation message should pop up
-    cy.on('window:alert', (message) => {
-      expect(message).to.equal('Product Fluid added successfully');
-    });
-});
-
-    it('should download all pricing', () => {
-        cy.login();
-
-        // Navigate to
-        cy.getPage('ProductStatic');
-
-        cy.intercept('GET', `${Cypress.env('BASE_URL')}/Analytics/genratePriceList`).as('downloadRequest');
-
-        // Trigger the download action (e.g., clicking a download button)
-        cy.get(`a[href="${Cypress.env('BASE_URL')}/Analytics/genratePriceList"]`).click();
-
-        // Wait for the download request to complete
-        cy.wait('@downloadRequest').then((interception) => {
-            expect(interception.response.statusCode).to.equal(200); // Check if the response status code is 200 (OK)
-
-            // TODO Neka look back into this
-            cy.log(interception.response);
+    // Fill out form        
+    cy.fixture('super-admin-v8').then(data => {
+        cy.handleDropdown('select#pricingPlanID', data.pricePlan.name);
+        cy.get('#planpricename').type(data.product.pricing.name2);
+        cy.get('label[for="isDefault"]').click();
+        
+        data.product.pricing.priceFluids[1].forEach((priceFluid, index) => {
+            cy.get(`#planAprice${index}`).type(priceFluid);
         });
+        cy.get('button.btn.btn-primary').contains('Ok').click()
     });
-});
+    
+    // Assert 
+    cy.wait('@addProductFluid1*').then(({response}) => {
+      const body = JSON.parse(response.body);
+      expect(response.statusCode).to.eq(200);
+      expect(body.error).to.eq(false);
+    });
+  });
+
+  it('should download all pricing', () => {
+       // Login and Navigate to Pricing
+      cy.login();
+      cy.getPage('ProductStatic');
+
+      cy.interceptApiCall('GET', 'Analytics/genratePriceList');
+
+      // Trigger the download action 
+      cy.get(`a[href="${(Cypress.env('BASE_URL'))}Analytics/genratePriceList"]`).click()
+      
+      // Assert
+      cy.wait('@genratePriceList').then(({response}) => {
+        expect(response.statusCode).to.eq(200);
+      });
+  });
+})
