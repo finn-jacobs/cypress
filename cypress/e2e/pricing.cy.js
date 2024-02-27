19c7b509782b5291d744e6a5fc6125e01a0262da
Anneika Weeks <anneikanicole@gmail.com>
2024-02-27 17:10:12 -0500

    add ar, layout and mfg term tests completed

--- cypress/e2e/pricing.cy.js
+++ cypress/e2e/pricing.cy.js
@@ -1,24 +1,4 @@
 describe('pricing testing', () => {
-    // TODO: Remove
-    // need this test in order to run the 'create new product test'
-    // it("should add a new carrier", () => {
-    //     // Login to Phoenix
-    //     cy.login();
-
-    //     // Navigate to Carrier
-    //     cy.getPage('carrier');
-
-    //     // Open add carrier modal
-    //     cy.get('div.card-header > div > div.col-md-4 > div > button').click().click();
-
-    //     // Fill form and submit
-    //     cy.fixture('super-admin-v8').then(data => {
-    //         cy.get('#term-group-1 > div > #value').type(data.carrier.name);
-    //         cy.get('#value-group-1 > div > #value').type(data.carrier.description);
-    //     });
-    //     cy.get('#modal-add___BV_modal_footer_ > button.btn-primary').click();
-    // });
-
     it('should create a new product', () => {
         cy.interceptApiCall('POST', 'ProductStatic/addProductStatic');
         cy.login();
---
+++ cypress/e2e/terms/add-terms.cy.js
@@ -0,0 +1,70 @@
+describe('terms', () => {
+    it('should add a new ar term', () => {
+        cy.interceptApiCall('POST', 'Terms/addTerm');
+
+        // login and navigate to terms page
+        cy.login();
+        cy.getPage('terms');
+
+        // open and fill out modal
+        cy.get('button.btn.base-button.btn-outline-default').click().click();
+        cy.fixture('super-admin-v8').then((data) => {
+            cy.handleDropdown('#term', data.term.aspectRatio);
+            cy.get("input#value").type('20:20');
+        });
+
+        // submit and assert
+        cy.get('button.btn-primary').click();
+        cy.wait('@addTerm').then(({ response }) => {
+            const body = JSON.parse(response.body);
+            expect(response.statusCode).to.eq(200);
+            expect(body.error).to.eq(false);
+        });
+    }); 
+
+    it('should add a new mfg term', () => {
+        cy.interceptApiCall('POST', 'Terms/addTerm');
+
+        // login and navigate to terms page
+        cy.login();
+        cy.getPage('terms');
+
+        // open and fill out modal
+        cy.get('button.btn.base-button.btn-outline-default').click().click();
+        cy.fixture('super-admin-v8').then((data) => {
+            cy.handleDropdown('#term', data.term.manufacturer);
+            cy.get("input#value").type('Cypress Manufacturer');
+        });
+
+        // submit and assert
+        cy.get('button.btn-primary').click();
+        cy.wait('@addTerm').then(({ response }) => {
+            const body = JSON.parse(response.body);
+            expect(response.statusCode).to.eq(200);
+            expect(body.error).to.eq(false);
+        });
+    }); 
+
+    it('should add a new layout term', () => {
+        cy.interceptApiCall('POST', 'Terms/addTerm');
+
+        // login and navigate to terms page
+        cy.login();
+        cy.getPage('terms');
+
+        // open and fill out modal
+        cy.get('button.btn.base-button.btn-outline-default').click().click();
+        cy.fixture('super-admin-v8').then((data) => {
+            cy.handleDropdown('#term', data.term.orientation);
+            cy.get("input#value").type('Cypress Test Layout');
+        });
+
+        // submit and assert
+        cy.get('button.btn-primary').click();
+        cy.wait('@addTerm').then(({ response }) => {
+            const body = JSON.parse(response.body);
+            expect(response.statusCode).to.eq(200);
+            expect(body.error).to.eq(false);
+        });
+    }); 
+});
